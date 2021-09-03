Fix a wrong order of keys issue in the add_point func

lux_ai/scraper.py
@@ -159,17 +159,22 @@ class Agent(abc.ABC):
             for i, (acts, acts_prob, obs) in enumerate(zip(actions_dict.values(),
                                                            actions_probs.values(),
                                                            proc_obs.values())):
-                for (k, action), (_, action_probs), (_, observation) in zip(acts.items(),
-                                                                            acts_prob.items(),
-                                                                            obs.items()):
+                acts = dict(sorted(acts.items()))
+                acts_prob = dict(sorted(acts_prob.items()))
+                obs = dict(sorted(obs.items()))
+                for (k1, action), (k2, action_probs), (k3, observation) in zip(acts.items(),
+                                                                               acts_prob.items(),
+                                                                               obs.items()):
+                    assert k1 == k2 == k3
                     value = [action, action_probs, actions_masks[i], observation]
-                    if k in player_data.keys():
-                        player_data[k].append(value, current_step)
+                    if k1 in player_data.keys():
+                        player_data[k1].append(value, current_step)
                     else:
-                        player_data[k] = tools.DataValue()
-                        player_data[k].append(value, current_step)
+                        player_data[k1] = tools.DataValue()
+                        player_data[k1].append(value, current_step)
             return player_data
 
+        step = 0
         for step in range(0, configuration.episodeSteps):
             assert observations[0]["updates"] == observations[1]["updates"] == data["steps"][step][0]["observation"][
                 "updates"]
@@ -230,6 +235,7 @@ class Agent(abc.ABC):
         progress = tf.linspace(0., 1., step + 2)[:-1]
         progress = tf.cast(progress, dtype=tf.float16)
 
+        reward1, reward2 = data["rewards"][0], data["rewards"][1]
         if reward1 > reward2:
             final_reward_1 = tf.constant(1, dtype=tf.float16)
             final_reward_2 = tf.constant(-1, dtype=tf.float16)

lux_ai/storage.py
@@ -1,7 +1,9 @@
-import tensorflow as tf
+from typing import List
 
 import reverb
-from typing import List
+import tensorflow as tf
+
+from lux_gym.envs.lux.action_vectors import action_vector
 
 
 class UniformBuffer:
@@ -14,7 +16,7 @@ class UniformBuffer:
                  checkpointer=None):
 
         OBSERVATION_SPEC = tf.TensorSpec(observations_shape, tf.float16)
-        ACTION_SPEC = tf.TensorSpec([39], tf.float16)
+        ACTION_SPEC = tf.TensorSpec([len(action_vector)], tf.float16)
 
         self._min_size = min_size
         self._table_names = [f"uniform_table_{i}" for i in range(num_tables)]

lux_ai/trainer.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import reverb
 
 from lux_ai import models, tools
+from lux_gym.envs.lux.action_vectors import action_vector
 
 physical_devices = tf.config.list_physical_devices('GPU')
 if len(physical_devices) > 0:
@@ -22,7 +23,7 @@ class Agent(abc.ABC):
         # self._lambda = config["lambda"]
 
         feature_maps_shape = tools.get_feature_maps_shape(config["environment"])
-        actions_shape = 39
+        actions_shape = len(action_vector)
 
         self._model = models.get_actor_critic(feature_maps_shape, actions_shape)
 
@@ -42,22 +43,22 @@ class Agent(abc.ABC):
         self._batch_size = config["batch_size"]
 
     def imitate_once(self):
-        # for sample in self._dataset.take(1):
-        #     actions = sample.data['actions']
-        #     action_v = actions.numpy()
-        #     actions_probs = sample.data['actions_probs']
-        #     action_probs_v = actions_probs.numpy()
-        #     actions_masks = sample.data['actions_masks']
-        #     actions_masks_v = actions_masks.numpy()
-        #     observations = sample.data['observations']
-        #     observations_v = observations.numpy()
-        #     total_rewards = sample.data['total_rewards']
-        #     total_rewards_v = total_rewards.numpy()
-        #     temporal_masks = sample.data['temporal_masks']
-        #     temporal_masks_v = temporal_masks.numpy()
-        #     progresses = sample.data['progresses']
-        #     progresses_v = progresses.numpy()
-        # info = self._client.server_info()
+        for sample in self._dataset.take(1):
+            actions = sample.data['actions']
+            actions_v = actions.numpy()
+            actions_probs = sample.data['actions_probs']
+            actions_probs_v = actions_probs.numpy()
+            actions_masks = sample.data['actions_masks']
+            actions_masks_v = actions_masks.numpy()
+            observations = sample.data['observations']
+            observations_v = observations.numpy()
+            total_rewards = sample.data['total_rewards']
+            total_rewards_v = total_rewards.numpy()
+            temporal_masks = sample.data['temporal_masks']
+            temporal_masks_v = temporal_masks.numpy()
+            progresses = sample.data['progresses']
+            progresses_v = progresses.numpy()
+        info = self._client.server_info()
 
         imitate_dataset = self._dataset.map(lambda x: ((tf.cast(x.data['observations'], dtype=tf.float32),
                                                         tf.cast(x.data['actions_masks'], dtype=tf.float32)),
@@ -68,7 +69,7 @@ class Agent(abc.ABC):
         batched_dataset = imitate_dataset.batch(self._batch_size, drop_remainder=True)
         dataset = batched_dataset.map(tools.merge_first_two_dimensions)
 
-        for sample in dataset.take(1):
+        for sample in batched_dataset.take(1):
             observations = sample[0][0].numpy()
             actions_masks = sample[0][1].numpy()
             actions_probs = sample[1][0].numpy()