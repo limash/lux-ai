89cbaa36417f450a7cd9b04b879f1a30dd4772a7
Limash <yakubov.sha@gmail.com>
2021-09-13 23:09:11 +0200

    Delete scan model, remove the last skip in skip connections model

--- lux_ai/models.py
+++ lux_ai/models.py
@@ -158,116 +158,6 @@ def actor_critic_separate():
     return model
 
 
-def actor_critic_with_scan():
-    import tensorflow as tf
-    import tensorflow.keras as keras
-
-    class ScanUnit(keras.layers.Layer):
-        def __init__(self, filters, initializer, size, **kwargs):
-            super().__init__(**kwargs)
-
-            height, width = size
-            self._filters = filters
-            self._conv_col = keras.layers.Conv2D(1, (int(height / 2), 1), padding="same",
-                                                 kernel_initializer=initializer, use_bias=False)
-            self._conv_row = keras.layers.Conv2D(1, (1, int(width / 2)), padding="same",
-                                                 kernel_initializer=initializer, use_bias=False)
-            self._conv = keras.layers.Conv2D(filters - 2, 3, kernel_initializer=initializer,
-                                             padding="same", use_bias=False)
-            self._norm = keras.layers.BatchNormalization()
-
-        def call(self, inputs, training=False, **kwargs):
-            x = inputs
-            y = self._conv_col(x)
-            z = self._conv_row(x)
-            o = self._conv(x)
-            x = tf.concat([y, z, o], axis=-1)
-            outputs = self._norm(x, training=training)
-            return outputs
-
-    class ResidualUnit(keras.layers.Layer):
-        def __init__(self, filters, initializer, activation, **kwargs):
-            super().__init__(**kwargs)
-
-            self._filters = filters
-            self._activation = activation
-            self._conv = keras.layers.Conv2D(filters, 3, kernel_initializer=initializer, padding="same", use_bias=False)
-            self._norm = keras.layers.BatchNormalization()
-
-        def call(self, inputs, training=False, **kwargs):
-            x = self._conv(inputs)
-            x = self._norm(x, training=training)
-            return self._activation(inputs + x)
-
-        def compute_output_shape(self, batch_input_shape):
-            batch, x, y, _ = batch_input_shape
-            return [batch, x, y, self._filters]
-
-    class ResidualModel(keras.Model):
-        def __init__(self, **kwargs):
-            super().__init__(**kwargs)
-
-            filters = 128
-            layers = 11
-
-            initializer = keras.initializers.VarianceScaling(scale=2.0, mode='fan_in', distribution='truncated_normal')
-            initializer_random = keras.initializers.random_uniform(minval=-0.03, maxval=0.03)
-            activation = keras.activations.relu
-
-            self._scan1 = ScanUnit(filters, initializer, (32, 32))
-            self._scan2 = ScanUnit(filters, initializer, (32, 32))
-            self._residual_block = [ResidualUnit(filters, initializer, activation) for _ in range(layers)]
-
-            self._city_tiles_probs0 = keras.layers.Dense(128, activation=activation, kernel_initializer=initializer)
-            self._city_tiles_probs1 = keras.layers.Dense(4, activation="softmax", kernel_initializer=initializer_random)
-            self._workers_probs0 = keras.layers.Dense(128, activation=activation, kernel_initializer=initializer)
-            self._workers_probs1 = keras.layers.Dense(19, activation="softmax", kernel_initializer=initializer_random)
-            self._carts_probs0 = keras.layers.Dense(128, activation=activation, kernel_initializer=initializer)
-            self._carts_probs1 = keras.layers.Dense(17, activation="softmax", kernel_initializer=initializer_random)
-
-            self._baseline = keras.layers.Dense(1, kernel_initializer=initializer_random,
-                                                activation=keras.activations.tanh)
-
-        def call(self, inputs, training=False, mask=None):
-            features, actions_mask = inputs
-
-            x = features
-
-            x = self._scan1(x)
-            x = self._scan2(x)
-
-            for layer in self._residual_block:
-                x = layer(x, training=training)
-
-            shape_x = tf.shape(x)
-            y = tf.reshape(x, (shape_x[0], -1, shape_x[-1]))
-            y = tf.reduce_mean(y, axis=1)
-
-            z = (x * features[:, :, :, :1])
-            shape_z = tf.shape(z)
-            z = tf.reshape(z, (shape_z[0], -1, shape_z[-1]))
-            z = tf.reduce_sum(z, axis=1)
-
-            t = self._city_tiles_probs0(z)
-            t = self._city_tiles_probs1(t)
-            w = self._workers_probs0(z)
-            w = self._workers_probs1(w)
-            c = self._carts_probs0(z)
-            c = self._carts_probs1(c)
-            probs = tf.concat([t, w, c], axis=1)
-            probs = probs * actions_mask
-
-            baseline = self._baseline(tf.concat([y, z], axis=1))
-
-            return probs, baseline
-
-        def get_config(self):
-            pass
-
-    model = ResidualModel()
-    return model
-
-
 def actor_critic_with_skip_connections():
     import tensorflow as tf
     import tensorflow.keras as keras
@@ -316,11 +206,7 @@ def actor_critic_with_skip_connections():
             self._norm2 = keras.layers.BatchNormalization()
             self._activation2 = keras.layers.ReLU()
 
-            self._residual_block3 = [ResidualUnit(filters, initializer, activation) for _ in range(3)]
-            self._conv3 = keras.layers.Conv2D(filters, 3, padding="same", kernel_initializer=initializer,
-                                              use_bias=False)
-            self._norm3 = keras.layers.BatchNormalization()
-            self._activation3 = keras.layers.ReLU()
+            self._residual_block3 = [ResidualUnit(filters, initializer, activation) for _ in range(4)]
 
             self._city_tiles_probs0 = keras.layers.Dense(128, activation=activation, kernel_initializer=initializer)
             self._city_tiles_probs1 = keras.layers.Dense(4, activation="softmax", kernel_initializer=initializer_random)
@@ -355,12 +241,8 @@ def actor_critic_with_skip_connections():
             x = self._norm2(x, training=training)
             x = self._activation2(s + x)
 
-            s = x
             for layer in self._residual_block3:
                 x = layer(x, training=training)
-            x = self._conv3(x)
-            x = self._norm3(x, training=training)
-            x = self._activation3(s + x)
 
             shape_x = tf.shape(x)
             y = tf.reshape(x, (shape_x[0], -1, shape_x[-1]))
