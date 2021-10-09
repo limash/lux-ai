3f03724f70306fc428cecbd22fdb1be1de7e604a
Shamil Yakubov <yakubov.sha@gmail.com>
2021-10-09 18:57:26 +0200

    Add an efficient2 model blocks from the official repository

--- lux_ai/models.py
+++ lux_ai/models.py
@@ -100,98 +100,63 @@ def actor_critic_residual(actions_shape):
 
 
 def actor_critic_efficient(actions_shape):
+    import copy
+    import itertools
+
     import tensorflow as tf
     import tensorflow.keras as keras
 
-    def swish(x):
-        return x * tf.nn.sigmoid(x)
-
-    class SEBlock(tf.keras.layers.Layer):
-        def __init__(self, input_channels, ratio=0.25):
-            super(SEBlock, self).__init__()
-            self.num_reduced_filters = max(1, int(input_channels * ratio))
-            self.pool = tf.keras.layers.GlobalAveragePooling2D()
-            self.reduce_conv = tf.keras.layers.Conv2D(filters=self.num_reduced_filters,
-                                                      kernel_size=(1, 1),
-                                                      strides=1,
-                                                      padding="same")
-            self.expand_conv = tf.keras.layers.Conv2D(filters=input_channels,
-                                                      kernel_size=(1, 1),
-                                                      strides=1,
-                                                      padding="same")
-
-        def call(self, inputs, **kwargs):
-            branch = self.pool(inputs)
-            branch = tf.expand_dims(input=branch, axis=1)
-            branch = tf.expand_dims(input=branch, axis=1)
-            branch = self.reduce_conv(branch)
-            branch = swish(branch)
-            branch = self.expand_conv(branch)
-            branch = tf.nn.sigmoid(branch)
-            output = inputs * branch
-            return output
-
-    class MBConv(tf.keras.layers.Layer):
-        def __init__(self, in_channels, out_channels, expansion_factor, stride, k, drop_connect_rate):
-            super(MBConv, self).__init__()
-            self.in_channels = in_channels
-            self.out_channels = out_channels
-            self.stride = stride
-            self.drop_connect_rate = drop_connect_rate
-            self.conv1 = tf.keras.layers.Conv2D(filters=in_channels * expansion_factor,
-                                                kernel_size=(1, 1),
-                                                strides=1,
-                                                padding="same")
-            self.bn1 = tf.keras.layers.BatchNormalization()
-            self.dwconv = tf.keras.layers.DepthwiseConv2D(kernel_size=(k, k),
-                                                          strides=stride,
-                                                          padding="same")
-            self.bn2 = tf.keras.layers.BatchNormalization()
-            self.se = SEBlock(input_channels=in_channels * expansion_factor)
-            self.conv2 = tf.keras.layers.Conv2D(filters=out_channels,
-                                                kernel_size=(1, 1),
-                                                strides=1,
-                                                padding="same")
-            self.bn3 = tf.keras.layers.BatchNormalization()
-            self.dropout = tf.keras.layers.Dropout(rate=drop_connect_rate)
-
-        def call(self, inputs, training=None, **kwargs):
-            x = self.conv1(inputs)
-            x = self.bn1(x, training=training)
-            x = swish(x)
-            x = self.dwconv(x)
-            x = self.bn2(x, training=training)
-            x = self.se(x)
-            x = swish(x)
-            x = self.conv2(x)
-            x = self.bn3(x, training=training)
-            if self.stride == 1 and self.in_channels == self.out_channels:
-                if self.drop_connect_rate:
-                    x = self.dropout(x, training=training)
-                x = tf.keras.layers.add([x, inputs])
-            return x
+    # import lux_ai.effnetv2_model as eff_model
+    import lux_ai.hparams as hparams
+    import lux_ai.effnetv2_configs as effnetv2_configs
+    import lux_ai.utils as utils
+    from lux_ai.effnetv2_model import round_filters, round_repeats, Stem, MBConvBlock, FusedMBConvBlock
 
     class EfficientModel(keras.Model):
         def __init__(self, actions_number, **kwargs):
-            super().__init__(**kwargs)
+            model_name = 'efficientnetv2-s'
+            super().__init__(name=model_name, **kwargs)
 
-            filters = 200
-            layers = 10
+            cfg = copy.deepcopy(hparams.base_config)
+            if model_name:
+                cfg.override(effnetv2_configs.get_model_config(model_name))
+            self.cfg = cfg
+            self._mconfig = cfg.model
+
+            self._stem = Stem(self._mconfig, self._mconfig.blocks_args[0].input_filters)
+
+            self._blocks = []
+            block_id = itertools.count(0)
+            block_name = lambda: 'blocks_%d' % next(block_id)
+            for block_args in self._mconfig.blocks_args:
+                assert block_args.num_repeat > 0
+                # Update block input and output filters based on depth multiplier.
+                input_filters = round_filters(block_args.input_filters, self._mconfig)
+                output_filters = round_filters(block_args.output_filters, self._mconfig)
+                repeats = round_repeats(block_args.num_repeat,
+                                        self._mconfig.depth_coefficient)
+                block_args.update(
+                    dict(
+                        input_filters=input_filters,
+                        output_filters=output_filters,
+                        num_repeat=repeats))
+
+                # The first block needs to take care of stride and filter size increase.
+                conv_block = {0: MBConvBlock, 1: FusedMBConvBlock}[block_args.conv_type]
+                self._blocks.append(
+                    conv_block(block_args, self._mconfig, name=block_name()))
+                if block_args.num_repeat > 1:  # rest of blocks with the same block_arg
+                    # pylint: disable=protected-access
+                    block_args.input_filters = block_args.output_filters
+                    block_args.strides = 1
+                    # pylint: enable=protected-access
+                for _ in range(block_args.num_repeat - 1):
+                    self._blocks.append(
+                        conv_block(block_args, self._mconfig, name=block_name()))
 
             initializer = keras.initializers.VarianceScaling(scale=2.0, mode='fan_in', distribution='truncated_normal')
             initializer_random = keras.initializers.random_uniform(minval=-0.03, maxval=0.03)
-            activation = keras.activations.relu
-
-            self._conv = keras.layers.Conv2D(filters, 3, padding="same", kernel_initializer=initializer, use_bias=False)
-            self._norm = keras.layers.BatchNormalization()
-            self._activation = keras.layers.ReLU()
-
-            self._efficient_block = [MBConv(in_channels=filters,
-                                            out_channels=filters,
-                                            expansion_factor=1,
-                                            stride=1,
-                                            k=3,
-                                            drop_connect_rate=0) for _ in range(layers)]
+            activation = utils.get_act_fn(self._mconfig.act_fn)
 
             self._depthwise = keras.layers.DepthwiseConv2D(13)
             self._flatten = keras.layers.Flatten()
@@ -199,26 +164,25 @@ def actor_critic_efficient(actions_shape):
             self._workers_probs0 = keras.layers.Dense(128, activation=activation, kernel_initializer=initializer)
             self._workers_probs1 = keras.layers.Dense(actions_number, activation="softmax",
                                                       kernel_initializer=initializer_random)
-
             self._baseline = keras.layers.Dense(1, kernel_initializer=initializer_random,
                                                 activation=keras.activations.tanh)
 
         def call(self, inputs, training=False, mask=None):
-            features = inputs
-            x = features
+            outputs = self._stem(inputs, training)
+            for idx, block in enumerate(self._blocks):
+                survival_prob = self._mconfig.survival_prob
+                if survival_prob:
+                    drop_rate = 1.0 - survival_prob
+                    survival_prob = 1.0 - drop_rate * float(idx) / len(self._blocks)
+                outputs = block(outputs, training=training, survival_prob=survival_prob)
 
-            x = self._conv(x)
-            x = self._norm(x, training=training)
-            x = self._activation(x)
-
-            for layer in self._efficient_block:
-                x = layer(x, training=training)
+            x = outputs
 
             shape_x = tf.shape(x)
             y = tf.reshape(x, (shape_x[0], -1, shape_x[-1]))
             y = tf.reduce_mean(y, axis=1)
 
-            z1 = (x * features[:, :, :, :1])
+            z1 = (x * inputs[:, :, :, :1])
             shape_z = tf.shape(z1)
             z1 = tf.reshape(z1, (shape_z[0], -1, shape_z[-1]))
             z1 = tf.reduce_sum(z1, axis=1)
@@ -238,4 +202,5 @@ def actor_critic_efficient(actions_shape):
             pass
 
     model = EfficientModel(actions_shape)
+    # model = eff_model.get_model("efficientnetv2-s", weights=None)
     return model
